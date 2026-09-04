9da7128c6e2392aba7d2f29a2a6f271d241358d2
Walodja1987 <wladimir.weinbender@gmail.com>
2026-09-04 07:30:24 +0200

    deployed to mainnet

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -1,5 +1,5 @@
 // XNS contract addresses by network
 export const XNS_ADDRESS: Record<string, string> = {
-  ethMain: "0xA235c204bf85BB8952720A64801992F883c9072E",
+  ethMain: "0xAb95DF5C44a18adb83e3cCC8972630d0392A4AEa",
   sepolia: "0xA235c204bf85BB8952720A64801992F883c9072E",
 };
